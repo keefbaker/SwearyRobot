0621708d9a4799589860d9c6e0a21455c2ddcea1
keefbaker <keef.baker@n5md.com>
2015-12-17 10:45:55 +0000

    added hollywood crawler for celeb lines

---
+++ holly.py
@@ -0,0 +1,9 @@
+import feedparser
+import random
+def celebgrab():
+	data = feedparser.parse('http://feeds.accesshollywood.com/AccessHollywood/LatestNews')
+	headlines = [str(i["title"]).replace("'","").replace('"',"").replace(":","") for i in data["entries"]]
+	words = [word for word in random.choice(headlines).split(" ")[0:2]]
+	return " ".join(words)
+if __name__ == "__main__":
+	print celebgrab()
