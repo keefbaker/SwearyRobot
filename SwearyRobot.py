3fcc35ea9dbfddeed2ff08dcb039df999da7df77
keefbaker <keef.baker@n5md.com>
2015-12-18 09:57:41 +0000

    extra bits

--- SwearyRobot.py
+++ SwearyRobot.py
@@ -2,6 +2,11 @@
 #
 # Sweary Robot by Keith Baker
 #
+# To get this to work for yourself (or use it in a similar way), you'll need modules:
+# feedparser and tweepy
+# And put your api credentials just as variables into a sweary_creds.py, variable names
+# are in the import statement below
+#
 import tweepy
 import random
 from holly import celebgrab
@@ -25,13 +30,14 @@ hashtags = [x['name'] for x in trends1[0]['trends'] if x['name'].startswith('#')
 #
 # It's combination list time!
 # 
-superlative = [ "Super", "Ultra", "Mega", "Ultra", "Weak", "Weepy", "Mad", "Rage", "Sad", "Farty", "Disgust-o-", "Bilious", "Pre-raphaelite-", "Vomito", "Arthurian-", "Bad", "cavernous", "Good", "Insdious", "Rupert-Murdochian-", "Cry", "Foxnews", "Stilted-", "Impossa", "Bleeding ", "Nostril", "Diahorretic", "Enochian ", "Cockripping ", "Sharp", "Evil", "Disturbing ", "Jerkwad ", "Screwy ", "Arseweeping-", "Scagged-up ", "Stink", "Gunked-up ", "Fisted ", "Smeared ", "Disappointing-", "Pus-filled ", "Botty", "Dingle", "Pissy", "Rancid ", "Rot", "Cacky", "Skank", "Funsize ", "Baby", "Ming", "Dodgy", "Fuck", "Skate", "Dirty", "Death", "Murder", "Frothing wrath", "Porn", "Micro", "Internet", "Massive", "Crotch", "Daily Mail ", "King", "Queen", "Princess", "Lantern-jawed-", "Crack", "Smeg", "Shitty", "Rat", "Lamplicking", "Incontinent ", "Trump", "Waste", "Gun-toting-", "Poo", "Milky", "Slime", "Colonic-", "Classically Greek ", "French rennaissance "]
-swear = [ "fuck", "cunt", "arse", "bum", "shit", "motherfucker", "piss", "wank", "vomit", "penis", "twat", "cock", "dumb", "fucknut", "cuntface", "dribble", "wipe", "chunder", "bastard", "mugfucker", "assclown", "bitch", "boner", "clitoris", "chode", "dickhead", "cuntrag", "dipshit", "douche", "nutsack", "pecker", "prick", "queef", "rimjob", "scrote", "shitbag", "skullfuck", "turd", "twatlips", "tit", "fart", "flaps", "jamrag", "bellend", "helmet", "dangler", "cocksnot", "jizz", "goatse", "paedo", "axe-wound", "anus", "brownhole", "dogfuck", "turd", "bottysausage", "pinwilly", "slurry", "asshole", "cackspider", "taint", "barse", "bollocks", "jerk", "residue", "scrotum", "fist", "puke", "crybaby", "pustule", "mom", "vagina", "testes", "marketing bastard", "telemarketer", "armpit hair", "sweaty ballsacks", "adobe FUCKING ACROBAT", "dogmuck" "David Cameron", "porridge", "filth", "scum", "slag", "stain", "drip", "widge", "pigeon", "bag", "hole", "boobs", "twerp", "cheeks", "fanny", "ejaculate", "weasel", "spank", "vibrator", "corpse", "bowels" ]
-preposition = [ "I mean %s,  right?", "Just %s!!", "There's %s.", "And there it is... %s.", "For the love of %s.", "For %s's sake!", "I'm raging about %s.", "It... %s just makes me sad.", "You're such a %s!", "This %s just isn't worth my time.", "I know I'm a bot, but %s, right?", "I can't take any more of this %s.", "I'm actually really %s pleased!", "I just ate some %s. It tasted awful!", "This can %s off!", "%s!" "Life would be better without %s, don't you think?", "This is a new design of %s, let me tell you!", "I need some %s for my garage, has anyone got some?", "Your face is a %s!", "Your mum's %s makes me sick.", "Stick %s up your massive arse!", "New %s, please.", "I could do with a %s.", "What this country needs is a new %s.", "I'm not arsed about your your %s, so shut your fucking gob.", "If only the %s was real..", "There's some %s on telly tonight.", "Instagrammed my %s. Not many likes. Feeling sad.", "Amazon have free %s. I paid loads for mine. ", "I am president %s. Fear me!", "20 times I said. %s yourself!" ]
-stuff = [ "tea", "toilet", "post office", "work canteen", "fish fingers", "Fenton pub", "salad bowl", "lemon juice", "helicopter cockpit", "DFS sale", "prison shower", "bolognese", "chistmas cake", "garden", "nude", "nearby branch of Tesco", "city of Whiterun", "dog's mouth", "spleen", "hotel towel", "cavernous arse of Alexander the Great", "martian rover", "photocopier outlet", "boot cupboard", "sellotape", "dogs mouth", "queen's ladygarden", "french mustard", "rotting turkey", "butthole of Santa", "dark one's testes", "nuclear waste barrel", "only copy of Half life 3", "last open Woolworths"]
-dang = [ "Enough of", "No more", "Had my fill of", "Please stop the", "Less of the", "So much for" ]
-dong = [ "So I said,", "Well he thought,", "I don't want", "He said to me,", "I never wanted to hear", "I asked for oil, I didn't say,", "Philosophy:" ]
-celebline = [ "I wish %s would %s %s!", "%s has %s'd in the %s. Disgraceful!", "I know %s is famous but %s a %s!", "%s, a classic %s %s", "Woo! %s #%s%s" ]
+superlative = [ "Super", "Ultra", "Mega", "Ultra", "Weak", "Weepy", "Mad", "Rage", "Sad", "Farty", "Disgust-o-", "Bilious", "Pre-raphaelite-", "Vomito", "Arthurian-", "Bad", "cavernous", "Good", "Insdious", "Rupert-Murdochian-", "Cry", "Foxnews", "Stilted-", "Impossa", "Bleeding ", "Nostril", "Diahorretic", "Enochian ", "Cockripping ", "Sharp", "Evil", "Disturbing ", "Jerkwad ", "Screwy ", "Arseweeping-", "Scagged-up ", "Stink", "Gunked-up ", "Fisted ", "Smeared ", "Disappointing-", "Pus-filled ", "Botty", "Dingle", "Pissy", "Rancid ", "Rot", "Cacky", "Skank", "Funsize ", "Baby", "Ming", "Dodgy", "Fuck", "Skate", "Dirty", "Death", "Murder", "Frothing wrath", "Porn", "Micro", "Internet", "Massive", "Crotch", "Daily Mail ", "King", "Queen", "Princess", "Lantern-jawed-", "Crack", "Smeg", "Shitty", "Rat", "Lamplicking", "Incontinent ", "Trump", "Waste", "Gun-toting-", "Poo", "Milky", "Slime", "Colonic-", "Classically Greek ", "French rennaissance ", "Redfaced", "Minging", "Milquetost", "Muckchucking ", "Vomitous"]
+swear = [ "fuck", "cum", "cunt", "arse", "bum", "shit", "motherfucker", "piss", "wank", "vomit", "penis", "twat", "cock", "dumb", "fucknut", "cuntface", "dribble", "wipe", "chunder", "bastard", "mugfucker", "assclown", "bitch", "boner", "clitoris", "chode", "dickhead", "cuntrag", "dipshit", "douche", "nutsack", "pecker", "prick", "queef", "rimjob", "scrote", "shitbag", "skullfuck", "turd", "twatlips", "tit", "fart", "flaps", "jamrag", "bellend", "helmet", "dangler", "cocksnot", "jizz", "goatse", "paedo", "axe-wound", "anus", "brownhole", "dogfuck", "turd", "bottysausage", "pinwilly", "slurry", "asshole", "cackspider", "taint", "barse", "bollocks", "jerk", "residue", "scrotum", "fist", "puke", "crybaby", "pustule", "mom", "vagina", "testes", "marketing bastard", "telemarketer", "armpit hair", "sweaty ballsacks", "adobe FUCKING ACROBAT", "dogmuck" "David Cameron", "porridge", "filth", "scum", "slag", "stain", "drip", "widge", "pigeon", "bag", "hole", "boobs", "twerp", "cheeks", "fanny", "ejaculate", "weasel", "spank", "vibrator", "corpse", "bowels" ]
+preposition = [ "I mean %s,  right?", "Just %s!!", "There's %s.", "And there it is... %s.", "For the love of %s.", "For %s's sake!", "I'm raging about %s.", "It... %s just makes me sad.", "You're such a %s!", "This %s just isn't worth my time.", "I know I'm a bot, but %s, right?", "I can't take any more of this %s.", "I'm actually really %s pleased!", "I just ate some %s. It tasted awful!", "This can %s off!", "%s!" "Life would be better without %s, don't you think?", "This is a new design of %s, let me tell you!", "I need some %s for my garage, has anyone got some?", "Your face is a %s!", "Your mum's %s makes me sick.", "Stick %s up your massive arse!", "New %s, please.", "I could do with a %s.", "What this country needs is a new %s.", "I'm not arsed about your your %s, so shut your fucking gob.", "If only the %s was real..", "There's some %s on telly tonight.", "Instagrammed my %s. Not many likes. Feeling sad.", "Amazon have free %s. I paid loads for mine. ", "I am president %s. Fear me!", "20 times I said. %s yourself!", "There's no %s here.", "Fuck a %s!!", "Nothing makes me %s like donuts.", "Start %sing NOW!", "I want to claw my own %s off!" ]
+stuff = [ "tea", "toilet", "post office", "work canteen", "fish fingers", "Fenton pub", "salad bowl", "lemon juice", "helicopter cockpit", "DFS sale", "prison shower", "bolognese", "chistmas cake", "garden", "nude", "nearby branch of Tesco", "city of Whiterun", "dog's mouth", "spleen", "hotel towel", "cavernous arse of Alexander the Great", "martian rover", "photocopier outlet", "boot cupboard", "sellotape", "dogs mouth", "queen's ladygarden", "french mustard", "rotting turkey", "butthole of Santa", "dark one's testes", "nuclear waste barrel", "only copy of Half life 3", "last open Woolworths", "word of God"]
+dang = [ "Enough of", "No more", "Had my fill of", "Please stop the", "Less of the", "So much for", "And then", "Don't talk to me about", "We've run out of" ]
+dong = [ "So I said,", "Well he thought,", "I don't want", "He said to me,", "I never wanted to hear", "I asked for oil, I didn't say,", "Philosophy:", "Why all the," ]
+celebline = [ "I wish %s would %s %s!", "%s has %s'd in the %s. Disgraceful!", "I know %s is famous but %s a %s!", "%s, a classic %s %s", "Woo! %s #%s%s", "Fuck %s, I'm %sed off with all the %s.", "Right, so %s. You know what I think? #%s%s" ]
+butscrewthis = [ "But screw this", "Great, more", "Bugger", "No point in", "Loving the", "I can't live without"]
 #
 # Time to construct the tweet
 #
@@ -48,11 +54,10 @@ elif random.randrange(20) < 9:
 else:
 	masstrack = random.choice(superlative) + random.choice(swear)
 	supermasstrack = random.choice(preposition) % masstrack
-	tweet = "%s %s But screw this %s." % (random.choice(dong),supermasstrack,random.choice(swear))
+	tweet = "%s %s %s %s." % (random.choice(dong),supermasstrack,random.choice(butscrewthis),random.choice(swear))
 
 #
 # And finally post the mother
 #
-if __name__ == "__main__":
-	if random.randrange(20) < 20:
-		api.update_status(tweet)
+if __name__ == "__main__" and random.randrange(20) < 5:
+	api.update_status(tweet)
