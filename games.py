beefd37af852d69be99aaadfe990b1574677f3ac
keefbaker <keef.baker@n5md.com>
2015-12-22 16:21:03 +0000

    Improved filter on games, extra bits

--- games.py
+++ games.py
@@ -3,9 +3,9 @@ import random
 def gamegrab():
 	data = feedparser.parse('http://www.amazon.co.uk/gp/rss/bestsellers/videogames/ref=zg_bs_videogames_rsslink')
 	headlines = [i["title"].encode("ascii", "ignore") for i in data["entries"]]
-	words = [word for word in random.choice(headlines).split(" ")[1:-1]]
-	if words[-1] == "(Xbox":
-		del words[-1]
+	badstuff = set("-()")
+	words = [word for word in random.choice(headlines).split(" ")[1:-1] if not badstuff.intersection(word)]
+
 	return " ".join(words)
 if __name__ == "__main__":
 	print gamegrab()
