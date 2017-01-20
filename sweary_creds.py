fe4ec7cf1cb4e175d635529e474dc98c9367b326
Keith Baker <keef.baker@n5md.com>
2017-01-20 20:59:18 +0000

    Create sweary_creds.py

---
+++ sweary_creds.py
@@ -0,0 +1,9 @@
+"""
+Pull through environment variables for creds
+"""
+import os
+
+apiKey = os.environ['apiKey']
+apiSecret = os.environ['apiSecret']
+accessToken = os.environ['accessToken']
+accessSecret = os.environ['accessSecret']
