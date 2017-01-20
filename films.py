bece1bd43b50e47cf4d084adc182ee0e7b2634c1
Keith Baker <keef.baker@n5md.com>
2017-01-20 11:27:51 +0000

    fixed pylint command

--- films.py
+++ films.py
@@ -11,7 +11,7 @@ def dvdgrab():
     headlines = [i["title"].encode("ascii", "ignore") for i in data["entries"]]
     brackets = set("[]()-")
     words = [
-        word for word in random.choice(headlines).split(" ")[1:-1] if not brackets.intersection(word) 
+        word for word in random.choice(headlines).split(" ")[1:-1] if not brackets.intersection(word)
         ]
     return " ".join(words)
 if __name__ == "__main__":
